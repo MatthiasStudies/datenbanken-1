0c2ddf82fb100624ea7f68cf71236f941bf6c7a1
Matthias Harzer <matthias.harzer03@gmail.com>
2024-10-28 08:21:26 +0100

    chore: adjust formatting

--- aufgabe4/src/main/java/de/hka/iwii/db1/jdbc/Main.java
+++ aufgabe4/src/main/java/de/hka/iwii/db1/jdbc/Main.java
@@ -12,14 +12,10 @@ public class Main {
         String databasePassword = "password";
         String databaseName = "jdbc";
 
-        // Load PostgreSQL driver
         Class.forName("org.postgresql.Driver");
 
-        // Connect to PostgreSQL server without specifying a database
         Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/" + defaultPGDatabase, databaseUser, databasePassword);
 
-
-        // Check if the database exists
         boolean databaseExists = false;
         try (Statement stmt = conn.createStatement(); ResultSet rs = stmt.executeQuery("SELECT 1 FROM pg_database WHERE datname = '" + databaseName + "'")) {
             if (rs.next()) {
@@ -45,44 +41,43 @@ public class Main {
     private static void printResult(ResultSet rs) throws SQLException {
         ResultSetMetaData meta = rs.getMetaData();
 
-        int numColumn = meta.getColumnCount();
-        int[] colSizes = new int[numColumn];
+        int numColumns = meta.getColumnCount();
+        int[] colSizes = new int[numColumns];
 
-        for (int i = 1; i <= numColumn; i++) {
+        for (int i = 1; i <= numColumns; i++) {
+            int headerColumnWidth = Math.max(meta.getColumnDisplaySize(i), meta.getColumnTypeName(i).length());
             // Using max label.length as fallback for short data-value columns
-            colSizes[i - 1] = Math.max(meta.getColumnLabel(i).length(), meta.getColumnDisplaySize(i));
+            colSizes[i - 1] = Math.max(meta.getColumnLabel(i).length(), headerColumnWidth);
         }
 
-        for (int i = 1; i <= numColumn; i++) {
+        for (int i = 1; i <= numColumns; i++) {
             String columnName = meta.getColumnName(i);
             int columnSize = colSizes[i - 1];
-            String optPipe = i == numColumn ? "" : "| ";
+            String optPipe = i == numColumns ? "" : "| ";
             System.out.printf("%-" + columnSize + "s" + optPipe, columnName);
         }
         System.out.println();
 
-
-        for (int i = 1; i <= numColumn; i++) {
+        for (int i = 1; i <= numColumns; i++) {
             String columnType = meta.getColumnTypeName(i);
             int columnSize = colSizes[i - 1];
-            String optPipe = i == numColumn ? "" : "| ";
+            String optPipe = i == numColumns ? "" : "| ";
             System.out.printf("%-" + columnSize + "s" + optPipe, columnType);
         }
         System.out.println();
 
-        for (int i = 1; i <= numColumn; i++) {
+        for (int i = 1; i <= numColumns; i++) {
             int columnSize = colSizes[i - 1];
-            String optPlus = i == numColumn ? "" : "+-";
-
+            String optPlus = i == numColumns ? "" : "+-";
             System.out.printf("%-" + columnSize + "s" + optPlus, "-".repeat(columnSize));
         }
         System.out.println();
 
         while (rs.next()) {
-            for (int i = 1; i <= numColumn; i++) {
+            for (int i = 1; i <= numColumns; i++) {
                 String columnValue = rs.getString(i);
                 int columnSize = colSizes[i - 1];
-                String optPipe = i == numColumn ? "" : "| ";
+                String optPipe = i == numColumns ? "" : "| ";
                 System.out.printf("%-" + columnSize + "s" + optPipe, columnValue);
             }
             System.out.println();
@@ -113,7 +108,7 @@ public class Main {
         }
     }
 
-    public static void main(String[] args) throws ClassNotFoundException, SQLException, IOException {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
         Connection conn = connect();
         JDBCBikeShop jdbcBikeShop = new JDBCBikeShop();
         jdbcBikeShop.reInitializeDB(conn);
